13ac1310708ee5373dd91dd65069955bb9f0ed75
Fabien Behra <fabien.behra@etu.univ-grenoble-alpes.fr>
2023-01-25 16:10:04 +0100

    Ajout Création topic

--- topicApp/src/app/home/home.page.ts
+++ topicApp/src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { CreateTopicComponent } from '../modals/create-topic/create-topic.component';
 import { Topic } from '../models/topic';
 import { TopicService } from '../services/topic.service';
 
@@ -9,25 +11,36 @@ import { TopicService } from '../services/topic.service';
 })
 export class HomePage {
 
-  constructor(private topicService: TopicService) {
+  constructor(private topicService: TopicService, private modalCtrl: ModalController) {
   }
 
-  fetchAllTopics(): Array<Topic>{
+  fetchAllTopics(): Array<Topic> {
     return this.topicService.findAll();
   }
 
-  deleteTopic(topic: Topic): void{
+  deleteTopic(topic: Topic): void {
     if (topic != undefined)
       return this.topicService.delete(topic);
   }
 
-  openTopic(topic: Topic){
-    if (topic != undefined){
+  openTopic(topic: Topic) {
+    if (topic != undefined) {
       //redirect to detail page
     }
   }
 
-  createTopic(){
-    //redirect to form
+  /**
+   * Ouvre la modal de création de topic, ajoute le topic créé après validation.
+   */
+  async createTopic() {
+    const modal = await this.modalCtrl.create({
+      component: CreateTopicComponent,
+    });
+    modal.present();
+
+    const { data, role } = await modal.onWillDismiss();
+    if (role === 'confirm') {
+        
+    }
   }
 }
--- topicApp/src/app/modals/create-topic/create-topic.component.ts
+++ topicApp/src/app/modals/create-topic/create-topic.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Post } from 'src/app/models/post';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { ModalController } from '@ionic/angular';
+import { PostService } from 'src/app/services/post.service';
+import { TopicService } from 'src/app/services/topic.service';
 
 @Component({
   selector: 'app-create-topic',
@@ -6,9 +11,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-topic.component.scss'],
 })
 export class CreateTopicComponent implements OnInit {
+  constructor(
+    private modalCtrl: ModalController, 
+    private postService : PostService, 
+    private topicService: TopicService) { }
 
-  constructor() { }
+  topicForm = new FormGroup({
+    name: new FormControl('', Validators.required),
+    posts: new FormControl(new Array())
+  });
+
+  availablePosts: Post[] = new Array()
 
   ngOnInit() {}
 
+  onSubmit() {
+    this.topicForm.value.name
+    console.warn(this.topicForm.value);
+  }
+
+  onWillDismiss(event: Event) { }
+
+  cancel() {
+    this.modalCtrl.dismiss(null, 'cancel')
+  }
+
+  confirm(){
+    if (!this.topicForm.valid) {
+      console.log('Please provide all the required values!')
+      return false;
+    } else {
+      const topic = this.topicService.create(this.topicForm.value.name ?? "", this.topicForm.value.posts ?? new Array())
+      this.modalCtrl.dismiss(topic, "confirm");
+      return true;
+    }  
+  }
 }
