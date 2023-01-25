e9192137ac66fc03e0f970b542d7041c230a0265
Fabien Behra <fabien.behra@etu.univ-grenoble-alpes.fr>
2023-01-25 16:09:45 +0100

    Update app.module.ts

--- topicApp/src/app/app.module.ts
+++ topicApp/src/app/app.module.ts
@@ -6,10 +6,13 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CreateTopicComponent } from './modals/create-topic/create-topic.component';
+import { CreatePostComponent } from './modals/create-post/create-post.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  declarations: [AppComponent, CreateTopicComponent],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, ReactiveFormsModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
