67f0ef25c662cbfd4efb6935d9833cf312e34ee1
Jad <jad.jawlakh7@gmail.com>
2023-03-05 19:24:09 +0100

    Implement Login form validation

--- topicApp/src/app/pages/login/login.module.ts
+++ topicApp/src/app/pages/login/login.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -9,12 +9,13 @@ import { LoginPageRoutingModule } from './login-routing.module';
 import { LoginPage } from './login.page';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    LoginPageRoutingModule
-  ],
+    imports: [
+        CommonModule,
+        FormsModule,
+        IonicModule,
+        LoginPageRoutingModule,
+        ReactiveFormsModule
+    ],
   declarations: [LoginPage]
 })
 export class LoginPageModule {}
--- topicApp/src/app/pages/login/login.page.ts
+++ topicApp/src/app/pages/login/login.page.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
+import {FormGroup, FormBuilder, Validators, FormControl, Form} from "@angular/forms";
+import {ToastController} from "@ionic/angular";
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  loginForm!: FormGroup;
+  isSubmitted = false;
+
+  private toastController = inject(ToastController);
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]],
+      password: ['', [Validators.required]]
+    })
   }
 
+  get errorControl() {
+    return this.loginForm.controls;
+  }
+
+  async login() {
+    this.isSubmitted = true;
+    if (!this.loginForm.valid) {
+      const toast = await this.toastController.create({
+        message: "Please make sure you provided all required values correctly.",
+        duration: 1500,
+        position: "bottom",
+        color: 'danger'
+      });
+      await toast.present();
+      console.log('Please provide all the required values!')
+      return false;
+    } else {
+      const toast = await this.toastController.create({
+        message: "Login successful.",
+        duration: 1500,
+        position: "bottom",
+        color: 'success'
+      });
+      await toast.present();
+      console.log(this.loginForm.value);
+      return true;
+    }
+  }
 }
