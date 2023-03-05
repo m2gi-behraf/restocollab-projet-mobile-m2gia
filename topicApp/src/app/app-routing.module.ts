293c557a16a4a5a6dc97a05825714fcf57ceacee
Jad <jad.jawlakh7@gmail.com>
2023-03-05 18:33:44 +0100

    Create Signup page

--- topicApp/src/app/app-routing.module.ts
+++ topicApp/src/app/app-routing.module.ts
@@ -19,6 +19,10 @@ export const routes: Routes = [
     path: 'login',
     loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
   },
+  {
+    path: 'signup',
+    loadChildren: () => import('./pages/signup/signup.module').then( m => m.SignupPageModule)
+  },
 ];
 
 @NgModule({
