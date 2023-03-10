b0030084eb3d01c406b448c40460b3d81f9c4505
Jad <jad.jawlakh7@gmail.com>
2023-03-10 20:13:23 +0100

    Home: implement search bar filtering feature
    Allowing us to filter our restaurants' list based on the name.
    If we have enough time we could possibly add advanced filters such as for: ranking, price, geographical proximity, etc...
    But for now this is good enough.

--- topicApp/src/app/pages/home/home.page.ts
+++ topicApp/src/app/pages/home/home.page.ts
@@ -6,18 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
-
+filterRestaurantName: any;
   restaurantsList = [
     {thumbnailURL: "../../assets/images/home/restaurant-la-ferme-a-dede.png", restaurantName: "La Ferme à Dédé", ranking: "4", cuisine: "🇫🇷", address:  "24 Rue Barnave, 38000 Grenoble"},
     {thumbnailURL: "../../assets/images/home/restaurant-au-liban.png", restaurantName: "Au Liban", ranking: "4", cuisine: "🇱🇧", address:  "16 Pl. Sainte-Claire, 38000 Grenoble"},
     {thumbnailURL: "../../assets/images/home/restaurant-comptoire-ditalie.png", restaurantName: "Comptoire d'Italie", ranking: "4", cuisine: "🇮🇹", address:  "4 Pl. de Gordes, 38000 Grenoble"},
   ]
 
-  //todo: implement service to fetch restaurants 
+  //todo: implement service to fetch restaurants
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  clickMe() {
+    console.log("yassss");
+  }
 }
