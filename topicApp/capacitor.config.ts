bbe84db48737ea7e51451428444227ae404ee2c2
Fabien Behra <fabien.behra@etu.univ-grenoble-alpes.fr>
2023-03-11 19:38:24 +0100

    Ajout OAuth keys

--- topicApp/capacitor.config.ts
+++ topicApp/capacitor.config.ts
@@ -4,7 +4,14 @@ const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'topicApp',
   webDir: 'www',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
+  plugins: {
+    GoogleAuth: {
+      scopes: ['profile', 'email'],
+      serverClientId: '322642081353-nq896as543vke18b7ifeqvuq3tj8p2m7.apps.googleusercontent.com',
+      forceCodeForRefreshToken: true,
+    },
+  },
 };
 
 export default config;
