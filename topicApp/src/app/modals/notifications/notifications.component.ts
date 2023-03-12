898a350816cd204bcd30ca2660f522d8addb07ea
Jad <18511463+jadjawlakh@users.noreply.github.com>
2023-03-12 19:04:58 +0100

    Notifications modal (#37)
    * Implement notifications modal
    
    Basic structure
    
    * Change number notifications
    
    Todo: change notification structure, once we have an notification structure/class that talks to the firestore side, we could easily intercept the number of notifications we have here instead of hard-coding it.
    
    For example we would have a Notifications class and inside it we'd have a getter function that returns the length of the notifications' list. Because at the moment the notificationsList is hardcoded in the notification.ts but this should be moved.
    
    * Header: make profile avatar clickable
    
    Leading to the user's profile across the board.

---
+++ topicApp/src/app/modals/notifications/notifications.component.ts
@@ -0,0 +1,51 @@
+import {Component, inject, OnInit} from '@angular/core';
+import {IonicModule, ModalController} from "@ionic/angular";
+import {ReactiveFormsModule} from "@angular/forms";
+import {NgForOf, NgIf} from "@angular/common";
+
+@Component({
+  selector: 'app-notifications',
+  standalone: true,
+  imports: [ReactiveFormsModule, IonicModule, NgIf, NgForOf],
+  templateUrl: './notifications.component.html',
+  styleUrls: ['./notifications.component.scss'],
+})
+export class NotificationsComponent implements OnInit {
+  private modalController = inject(ModalController);
+  notificationsList = [
+    {type: "friend-request", message: "Laura sent you a friend request!"},
+    {type: "read-write-invitation", message: "Jad invited you to collaborate on a restaurant list!"},
+    {type: "read-only-invitation", message: "Josh invited you to view his restaurant list!"},
+  ]; //todo: create a notification class/model with relevant properties and getters
+
+  constructor() { }
+
+  ngOnInit() {}
+
+  dismissModal() {
+    this.modalController.dismiss();
+  }
+
+  goToNotification(notificationType: string) {
+    // todo: implement navigation to relevant/concerned page/section of said notification
+    // todo: would be interesting to implement an interface/class/enum class for the notification object when setting up the service
+    switch (notificationType) {
+      case "friend-request": {
+        console.log("it's a friend request");
+        break;
+      }
+      case "read-only-invitation": {
+        console.log("it's a read-only invitation!");
+        break;
+      }
+      case "read-write-invitation": {
+        console.log("it's a read-write invitation!");
+        break;
+      }
+      default: {
+        break;
+      }
+    }
+    console.log(notificationType);
+  }
+}
