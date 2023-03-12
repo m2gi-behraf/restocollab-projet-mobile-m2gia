9f71c7ce8de4e3ab2bfd22df5a61b335ade2064e
Jad <jad.jawlakh7@gmail.com>
2023-03-12 16:48:36 +0100

    User profile: implement logout button
    Display success toast and go back to login page.
    
    Todo: implement the actual backend part of logging out of an account.

--- topicApp/src/app/pages/user-profile/user-profile.page.ts
+++ topicApp/src/app/pages/user-profile/user-profile.page.ts
@@ -1,5 +1,5 @@
 import {Component, inject, OnInit} from '@angular/core';
-import {ModalController} from "@ionic/angular";
+import {ModalController, NavController, ToastController} from "@ionic/angular";
 import {ConfirmDeletionComponent} from "../../modals/confirm-deletion/confirm-deletion.component";
 
 @Component({
@@ -9,9 +9,9 @@ import {ConfirmDeletionComponent} from "../../modals/confirm-deletion/confirm-de
 })
 export class UserProfilePage implements OnInit {
   private modalController = inject(ModalController);
+  private toastController = inject(ToastController);
 
-
-  constructor() { }
+  constructor(public navigationControl: NavController) { }
 
   ngOnInit() {
   }
@@ -23,5 +23,18 @@ export class UserProfilePage implements OnInit {
     modal.present();
 
     const {data, role} = await modal.onWillDismiss();
+  }
+
+  async logout() {
+    // todo terminate account session via firebase
+    this.toastController.create({
+      message: "Account session successfully terminated. You are now logged out.",
+      duration: 1500,
+      position: "bottom",
+      color: 'success'
+    }).then(async (toast) => {
+      await toast.present();
+    });
+    await this.navigationControl.navigateRoot('login');
   }
 }
