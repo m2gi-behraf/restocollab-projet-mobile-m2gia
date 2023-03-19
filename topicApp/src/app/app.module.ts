b7ceeab165169863530a5485e97b06251e0d110c
Jad <jad.jawlakh7@gmail.com>
2023-03-19 14:38:43 +0100

    [NPM-Bug-Fix] Fix issues related to social sharing module
    Where app used to crash, now the problem seems to be resolved.
    
    To test the sharing functionality, please run the app in simulation mode via command: 'ionic capacitor build android'.

--- topicApp/src/app/app.module.ts
+++ topicApp/src/app/app.module.ts
@@ -11,7 +11,7 @@ import { provideFirebaseApp, initializeApp } from '@angular/fire/app'
 import { provideFirestore, getFirestore } from '@angular/fire/firestore'
 import { provideAuth, getAuth } from '@angular/fire/auth'
 import { environment } from 'src/environments/environment'
-import { SocialSharing } from '@awesome-cordova-plugins/social-sharing/ngx';
+import { SocialSharing } from "@awesome-cordova-plugins/social-sharing/ngx";
 
 @NgModule({
   declarations: [AppComponent],
