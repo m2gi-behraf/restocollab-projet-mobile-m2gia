4e661a75cb379d38d62c97462e7bed8f89f3a58f
Fabien Behra <fabien.behra@etu.univ-grenoble-alpes.fr>
2023-03-27 19:32:10 +0200

    remove emails usages

--- topicApp/src/app/modals/share-restaurant/share-restaurant.component.ts
+++ topicApp/src/app/modals/share-restaurant/share-restaurant.component.ts
@@ -1,5 +1,5 @@
 import {Component, inject, OnInit} from '@angular/core';
-import {IonicModule, ModalController} from "@ionic/angular";
+import {IonicModule, ModalController, ToastController} from "@ionic/angular";
 import { SocialSharing} from "@awesome-cordova-plugins/social-sharing/ngx";
 import {Restaurant} from "../../models/Restaurant";
 @Component({
@@ -12,36 +12,13 @@ import {Restaurant} from "../../models/Restaurant";
 export class ShareRestaurantComponent implements OnInit {
   private modalController = inject(ModalController);
   restaurant: Restaurant = {} as Restaurant;
+  private toastController = inject(ToastController);
   constructor(private socialSharing: SocialSharing) { }
-
   ngOnInit() {}
-
   dismissModal() {
     this.modalController.dismiss();
   }
-
   shareElement(smedia: any) {
-
-    // This feature is left out at the moment because you need to run it
-    // on a device that actually has WhatsApp installed on it.
-
-    // if (smedia === "email") {
-    //   this.socialSharing.shareViaEmail("Restaurant name: " + this.restaurant.name
-    //     + "\nRanking: " + this.restaurant.ranking
-    //     + "\nCuisine: " + this.restaurant.cuisine
-    //     + "\nAddress: " + this.restaurant.address
-    //     + "\nDescription: " + this.restaurant.description,
-    //     "I'd like to share a cool restaurant with you!", ["insert@email.here"],undefined,undefined, this.restaurantThumbnailURL
-    //   ).then((res) => {
-    //     console.log("Share via Email successful!");
-    //   }).catch((error) => {
-    //     alert(JSON.stringify(error));
-    //   })
-    // }
-
-    if (smedia == "email")
-      console.log("Sharing via email is coming in v2.0");
-
     if (smedia == "sms") {
       this.socialSharing.shareViaSMS("Hey, I'd like to share this cool restaurant with you!"
         + "\nRestaurant name: " + this.restaurant.name
@@ -56,24 +33,14 @@ export class ShareRestaurantComponent implements OnInit {
       })
     }
 
-    // This feature is left out at the moment because you need to run it
-    // on a device that actually has WhatsApp installed on it.
-
-    // if (smedia == "whatsapp") {
-    //   this.socialSharing.shareViaWhatsApp("Hey, I'd like to share this cool restaurant with you!"
-    //     + "\nRestaurant name: " + this.restaurant.name
-    //     + "\nRanking: " + this.restaurant.ranking
-    //     + "\nCuisine: " + this.restaurant.cuisine
-    //     + "\nAddress: " + this.restaurant.address
-    //     + "\nDescription: " + this.restaurant.description,
-    //     this.restaurantThumbnailURL).then((res) => {
-    //       console.log("Share via Whatsapp successful!");
-    //   }).catch((error) => {
-    //     alert(JSON.stringify(error));
-    //   })
-    // }
-
     if (smedia == "whatsapp")
-      console.log("Sharing via Whatsapp is coming in v2.0");
+      this.toastController.create({
+        message: "Sharing via Whatsapp is coming in v2.0",
+        duration: 1500,
+        position: "bottom",
+        color: 'medium'
+      }).then(async (toast) => {
+        await toast.present();
+      });
   }
 }
